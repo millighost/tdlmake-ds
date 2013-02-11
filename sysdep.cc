89b2c806bb90b657a050f609eb05071fe53d1214
millighost <rassahah@neofonie.de>
2013-02-11 22:18:19 +0100

    added function to look for common folder

--- sysdep.cc
+++ sysdep.cc
@@ -14,6 +14,35 @@ static const char log_filename[] = "v:/tmp/log.txt";
 static const char log_filename[] = "log.txt";
 #endif
 
+#if defined (WIN32)
+/**
+ * retrieve the common documents folder.
+ */
+static std::string get_common_folder ()
+{
+  std::string folder_name;
+  HKEY hkey;
+  if (RegOpenKeyEx (HKEY_LOCAL_MACHINE,
+      "Software\\Microsoft\\Windows\\CurrentVersion"
+      "\\Explorer\\Shell Folders\\", 0, KEY_READ, &hkey) != 0) {
+    return std::string ();
+  }
+  {
+    char buffer[1000];
+    DWORD buffer_size = sizeof buffer;
+    DWORD type;
+    if (RegQueryValueEx (hkey, "Common Documents", 0, &type,
+        (BYTE *) &buffer[0], &buffer_size) == 0)
+      {
+        folder_name.assign (buffer, buffer_size);
+        folder_name += '\\';
+      }
+  }
+  RegCloseKey (hkey);
+  return folder_name;
+}
+#endif
+
 /**
  * get the path directory of the executable.
  */
@@ -65,13 +94,26 @@ std::string get_path_tdlmake (const std::string &self)
   return self + "tdlmake-org.exe";
 }
 
+/**
+ * get the configuration filename.
+ */
+static std::string get_datafile (const std::string &selfdir)
+{
+#if defined (WIN32)
+  std::string common_folder (get_common_folder ());
+  return common_folder + "gamma.txt";
+#else
+  return selfdir + "gamma.txt";
+#endif
+}
+
 } /* local namespace */
 
 #include <iostream>
 sysdep_conf::sysdep_conf (const std::string &argv0)
   : path_self (get_path_self (argv0)),
     path_tdlmake (get_path_tdlmake (path_self)),
-    path_datafile (path_self + "gamma.txt"),
+    path_datafile (get_datafile (path_self)),
     path_logfile (log_filename)
 {
 }
